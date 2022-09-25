74020cf09e6710ef75fddb4e5e0be8640b635527
panlinnuo <58615088+panlinnuo@users.noreply.github.com>
2022-09-25 10:37:37 +0800

    第五课作业

--- src/goodbye_backend/main.mo
+++ src/goodbye_backend/main.mo
@@ -1,5 +1,125 @@
+import List "mo:base/List";
+import Iter "mo:base/Iter";
+import Principal "mo:base/Principal";
+import Time "mo:base/Time";
+import Nat "mo:base/Nat";
+
 actor {
-  public func greet(name : Text) : async Text {
-    return "Hello, " # name # "!";
+  public type Message = {
+    author: Text;
+    text: Text; 
+    time: Time.Time;
   };
+
+  var king: Text = "Linnuo";
+
+  public type Microblog = actor {
+    follow: shared(Principal) -> async (); // add关注对象
+    follows: shared query () -> async [Principal]; // return关注对象列表
+    post: shared (Text) -> async (); // 发布新消息
+    posts: shared (Time.Time) -> async [Message]; // return 发布消息列表
+    posts2: shared (Text) -> async [Message]; // return 发布消息列表
+    timeline: shared (Time.Time, Text) -> async [Message]; // return 所有关注对象发布的消息
+    set_name: shared (Text) -> async ();  // set 名字
+    get_name: shared query () -> async ?Text; // get 名字
+    follows_name: shared () -> async [?Text]; // retuen 跟随者名称列表
+    unfollow: shared () -> async ();  // 清空跟随
+  };
+
+  // stable 修饰：升级不清空内存
+  stable var followed : List.List<Principal> = List.nil();
+
+  public shared func follow(id: Principal) : async (){
+    followed := List.push(id, followed);
+  };
+
+  public shared query func follows() : async [Principal] {
+    List.toArray(followed);
+  };
+
+  stable var messages : List.List<Message> = List.nil();
+
+  // （msg）：获取消息属性
+  public shared (msg) func post(pwd: Text, text: Text) : async (){
+    // 获取发送者: dfx identity get-principal
+    // assert(Principal.toText(msg.caller) == "vw7ov-537vk-abslh-s2gx7-gw2ff-v4u6y-thlcs-hejxf-hkkc5-bjiq7-pqe"); //消息发送者
+    assert(pwd == "qwe123");
+    let m = {
+      author = king;
+      text = text;
+      time = Time.now();
+    };
+    messages := List.push(m, messages);
+    // 用钱包调用正常返回失败：dfx canister --wallet=$(dfx identity get-wallet) call microblog_backend post "(\"Second post\")"
+  };
+
+  public shared func posts(since: Time.Time) : async [Message] {
+    var list : List.List<Message> = List.nil();
+
+    for (m in Iter.fromList(messages)) {
+      if (m.time >= since){
+        list := List.push(m, list);
+      }
+    };
+
+    List.toArray(list);
+  };
+
+  public shared func posts2(user: Text) : async [Message] {
+    var list : List.List<Message> = List.nil();
+
+    for (m in Iter.fromList(messages)) {
+      if (m.author == user){
+        list := List.push(m, list);
+      }
+    };
+
+    List.toArray(list);
+  };
+
+  public shared func timeline(since: Time.Time, user: Text) : async [Message] {
+    var all : List.List<Message> = List.nil();
+
+    for (id in Iter.fromList(followed)){
+      let canister : Microblog = actor(Principal.toText(id));
+      let msgs = await canister.posts(since);
+
+      for(msg in Iter.fromArray(msgs)){
+        all := List.push(msg, all);
+      };
+    };
+
+    List.toArray(all);
+  };
+
+  public shared func set_name(name: Text) {
+    king := name;
+  };
+
+  public shared query func get_name() : async ?Text {
+    return ?king;
+  };
+
+  public shared func follows_name() : async [?Text] {
+    var names : List.List<?Text> = List.nil();
+
+    for (id in Iter.fromList(followed)){
+      let canister: Microblog = actor(Principal.toText(id));
+      let name: ?Text = await canister.get_name();
+      names := List.push(name, names);
+    };
+    List.toArray(names);
+  };
+
+  public shared func unfollow() : async (){
+    followed := List.nil();
+  }
+
+  // 发送消息：id和名称可以呼唤
+  //  dfx canister call rrkah-fqaaa-aaaaa-aaaaq-cai post "(\"First post\")"   // 用Id发消息
+  //  dfx canister call microblog_backend post "(\"Second post\")"            // 用名称发消息
+  //  dfx canister call rrkah-fqaaa-aaaaa-aaaaq-cai posts "()"                // 获取消息列表
+  //  dfx canister call microblog_backend2 follow "(principal \"$(dfx canister id microblog_backend)\")"  // 添加关注对象
+  //  dfx canister call microblog_backend2 follows "()"                       // 获取关注对象列表
+  //  dfx canister call microblog_backend2 timeline  "()"                     // 获取所有关注对象发布的消息
 };
--- src/goodbye_frontend/src/index.js
+++ src/goodbye_frontend/src/index.js
@@ -1,19 +1,112 @@
+import { Int } from "@dfinity/candid/lib/cjs/idl";
 import { goodbye_backend } from "../../declarations/goodbye_backend";
 
-document.querySelector("form").addEventListener("submit", async (e) => {
-  e.preventDefault();
-  const button = e.target.querySelector("button");
+async function post() {
+  let post_button = document.getElementById("post");
+  let err = document.getElementById("error");
+  err.innerText = "";
+  post_button.ariaDisabled = true;
+  let textarea = document.getElementById("message");
+  let text = textarea.value;
+  let pwd = document.getElementById("pwd").value;
+  try { 
+    await goodbye_backend.post(pwd, text);
+    textarea.value = "";
+  } catch (error) {
+    console.error(error);
+    err.innerText = "Failed";
+  }
+  post_button.ariaDisabled = false;
+}
 
-  const name = document.getElementById("name").value.toString();
+var num_posts = 0;
+async function load_posts() {
+  let posts_section = document.getElementById("posts");
+  let posts = await goodbye_backend.posts(1);
+  if (num_posts == posts.length) return;
+  posts_section.replaceChildren([]);
+  num_posts = posts.length;
+  for(var i = num_posts - 1; i >= 0; i --) {
+    let post = document.createElement("p");
+    post.innerText = posts[i].author + " SAY: " +  posts[i].text + " - " + Date((BigInt(posts[i].time) / BigInt(1000000000))).toLocaleString(); 
+    posts_section.appendChild(post);
+  }
+}
 
-  button.setAttribute("disabled", true);
+async function getName() {
+  let get_button = document.getElementById("getName");
+  get_button.disabled = true;
+  let name = await goodbye_backend.get_name();
+  document.getElementById("name").value = name;
+  get_button.disabled = false;
+}
 
-  // Interact with foo actor, calling the greet method
-  const greeting = await goodbye_backend.greet(name);
+async function setName() {
+  let set_button = document.getElementById("setName");
+  let info = document.getElementById("info");
+  info.innerText = "";
+  set_button.disabled = true;
+  let nameIn = document.getElementById("name");
+  let name = nameIn.value;
+  try { 
+    await goodbye_backend.set_name(name);
+    nameIn.value = "";
+    info.innerText = "Success";
+  } catch (error) {
+    console.error(error);
+    info.innerText = "Failed";
+  }
+  set_button.disabled = false;
+}
 
-  button.removeAttribute("disabled");
+var num_follows = 0;
+async function follow() {
+  let follows = await goodbye_backend.follows_name();
+  if (num_follows == follows.length) return;
+  num_follows = follows.length;
+  let info = "";
+  for(var i = 0; i < num_follows; i ++) {
+    let id = follows[i];
+    info += "<button class='info_btn' id= " + id + ">" + id + "</button>";
+    info += "<div id=" + id + "_msg" + "></div>";
+    document.getElementById("follow").innerHTML = info;  
+    document.getElementById(id).addEventListener('click', function handleClick(event) {
+      load_posts2(id);
+    })
+  }
+}
 
-  document.getElementById("greeting").innerText = greeting;
+var num_posts2 = 0;
+async function load_posts2(user) {
+  let posts_section2 = document.getElementById(user + "_msg");
+  try {
+    let posts2 = await goodbye_backend.posts2('"' + user + '"');
+    if (num_posts2 == posts2.length) return;
+    posts_section2.replaceChildren([]);
+    num_posts2 = posts2.length;
+    for(var i = num_posts2 - 1; i >= 0; i --) {
+      let post2 = document.createElement("p");
+      post.innerText = posts2[i].author + " SAY: " +  posts2[i].text + " - " + Date((BigInt(posts2[i].time) / BigInt(1000000000))).toLocaleString(); 
+      posts_section2.appendChild(post2);
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
 
-  return false;
-});
+async function load() {
+  follow();
+
+  let post_button = document.getElementById("post");
+  post_button.onclick = post;
+  load_posts();
+  setInterval(load_posts, 5000);
+
+  let get_button = document.getElementById("getName");
+  get_button.onclick = getName;
+
+  let set_button = document.getElementById("setName");
+  set_button.onclick = setName;
+}
+
+window.onload = load;
